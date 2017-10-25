198c3446aebeb00ff708911ce5ed8a95c1ebc9e4
Aaron Macdonald <aaronmac@clear.net.nz>
2017-10-25 23:21:05 +1300

    next pair working

--- src/core.js
+++ src/core.js
@@ -4,8 +4,19 @@ export function setEntries(state, entries) {
     return state.set('entries', List(entries))
 }
 
+function getWinners(vote) {
+    if(!vote) return []
+    const [a, b] = vote.get('pair')
+    const aVotes = vote.getIn(['tally', a], 0)
+    const bVotes = vote.getIn(['tally', b], 0)
+    if (aVotes > bVotes) return [a]
+    else if (aVotes < bVotes) return [b] 
+    else return [a, b]
+}
+
 export function next(state) {
     const entries = state.get('entries')
+                         .concat(getWinners(state.get('vote')))  
     return state.merge({
         vote: Map({pair:entries.take(2)}),
         entries: entries.skip(2)
@@ -19,3 +30,4 @@ export function vote(state, entry) {
         tally => tally + 1
     )
 }
+
